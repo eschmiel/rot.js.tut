f5efac96dd68cbcb5fee56a392ffaa3ebe858462
Eric Schmiel <eschmiel@gmail.com>
2020-03-03 14:44:20 -0500

    Finished part 2 of the tutorial

--- game.js
+++ game.js
@@ -1,13 +1,69 @@
 var Game = {
 	display: null,
 	
+	engine: null,
+	player: null,
 	init: function() {
+		
+		
 		this.display = new ROT.Display();
 		document.body.appendChild(this.display.getContainer());
 		
 		this._generateMap();
+		var scheduler = new ROT.Scheduler.Simple();
+		scheduler.add(this.player, true);
+		this.engine = new ROT.Engine(scheduler);
+		this.engine.start();
 	}
 }
+
+var Player = function (x, y) { // a function that generates a player
+	this._x = x;
+	this._y = y;
+	this._draw();
+}
+
+Player.prototype._draw= function() { // Creates the draw function for the player object
+	Game.display.draw(this._x, this._y, "@", "#ff0");
+}
+
+Player.prototype.act = function() {
+	Game.engine.lock();
+	
+	window.addEventListener("keydown", this);
+}
+
+Player.prototype.handleEvent = function(e) {
+	var keyMap = {};
+	keyMap[38] = 0;
+	keyMap[33] = 1;
+	keyMap[39] = 2;
+	keyMap[34] = 3;
+	keyMap[40] = 4;
+	keyMap[35] = 5;
+	keyMap[37] = 6;
+	keyMap[36] = 7;
+	
+	var code = e.keyCode;
+	
+	if(!(code in keyMap)) {return;}
+	
+	var diff = ROT.DIRS[8][keyMap[code]];
+	var newX = this._x + diff[0];
+	var newY = this._y + diff[1];
+	
+	var newKey = newX + "," + newY;
+	if(!(newKey in Game.map)) {return;}
+	
+	Game.display.draw(this._x, this._y, Game.map[this._x + ',' + this._y]);
+	this._x = newX;
+	this._y = newY;
+	this._draw();
+	window.removeEventListener("keydown", this);
+	Game.engine.unlock();
+}
+
+
 
 Game.map = {};
 Game._generateMap = function() {
@@ -25,6 +81,8 @@ Game._generateMap = function() {
 	digger.create(digCallback.bind(this));
 	this._generateBoxes(freeCells);
 	this._drawWholeMap();
+	
+	this._createPlayer(freeCells);
 }
 
 Game._drawWholeMap = function() {
@@ -42,4 +100,13 @@ Game._generateBoxes = function (freeCells) {
 		var key = freeCells.splice(index, 1)[0];
 		this.map[key] = "*";
 	}
+}
+
+Game._createPlayer = function(freeCells) {
+	var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+	var key = freeCells.splice(index, 1)[0];
+	var parts = key.split(",");
+	var x = parseInt(parts[0]);
+	var y = parseInt(parts[1]);
+	this.player = new Player(x, y);
 }
